047bf69a1943afc26272e216e79e19f683e1932e
Fahim Shareear <fahimshareear@gmail.com>
2025-11-23 04:59:26 +0600

    Adding the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './Componants/Countries'
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all#')
+  .then(res => res.json())
+
 
 function App() {
 
   return (
     <>
-      <Countries></Countries>      
+      <Suspense fallback={<p>Nadir On the go loading..........</p>}>
+          <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>   
     </>
   )
 }
--- src/Componants/Countries.jsx
+++ src/Componants/Countries.jsx
@@ -1,8 +1,13 @@
-export default function Countries(){
+import { use } from "react"
+
+export default function Countries({countriesPromise}){
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+    console.log(countries)
     return(
         <>
             <div>
-                <h1>In the world</h1>
+                <h1>In the world {countries.length}</h1>
             </div>
         </>
     )
