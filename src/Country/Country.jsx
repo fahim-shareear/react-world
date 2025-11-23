b28f20f3e630b325e191bc01595fbb8be1ae6b69
Fahim Shareear <fahimshareear@gmail.com>
2025-11-23 23:51:05 +0600

    Adding rest of the information

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -7,6 +7,10 @@ const Country = ({country}) => {
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Continant: {country.continents.continents}</p>
+            <p>Region: {country.region.region}</p>
+            <p>Area: {country.area.area}</p>
+            <p></p>
         </div>
     );
 };
