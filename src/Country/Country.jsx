572eb9b619e825bb841acad6418c4c31cc4f347b
Fahim Shareear <fahimshareear@gmail.com>
2025-11-23 23:32:09 +0600

    Adding Country Flags from API

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    console.log(country)
+    console.log(country.flags.flags.png)
     return (
         <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
         </div>
     );
