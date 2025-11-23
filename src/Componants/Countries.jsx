09691a1a01f84c99782753d9af9a28020191b7a2
Fahim Shareear <fahimshareear@gmail.com>
2025-11-23 23:20:56 +0600

    Adding another componanat to use & map the API data

--- src/Componants/Countries.jsx
+++ src/Componants/Countries.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({countryPromise}) => {
     const countryData = use(countryPromise);
@@ -6,6 +7,9 @@ const Countries = ({countryPromise}) => {
     return (
         <div>
             <h1>The total number of countries in the world is: {country.length}</h1>
+            {
+                country.map(country => <Country country={country}></Country>)
+            }
         </div>
     );
 };
