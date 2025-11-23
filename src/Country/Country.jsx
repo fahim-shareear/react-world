43e9dd3457558234486c64dc21968c8318f9201b
Fahim Shareear <fahimshareear@gmail.com>
2025-11-23 23:38:51 +0600

    Adding flag & Population from the API

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    console.log(country.flags.flags.png)
+    console.log(country.population.population);
     return (
         <div>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
         </div>
     );
 };
