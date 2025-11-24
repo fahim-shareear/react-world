c502b3a29f9d91dd5d85a0471387e428a99d72d4
arctic-shark <fahimshareear@gmail.com>
2025-11-25 02:15:54 +0600

    Re pushing to git

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
+
+    const handleVisited = () =>{
+        console.log("Visited");
+    }
+
+
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h1>Name: {country.name.common}</h1>
             <h1>Official Name: {country.name.official}</h1>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? 'Large Country' : 'Small Country'}</p>
+            <p>Capital: {country.capital.capital}</p>
+            <p>Region: {country.region.region}</p>
+            <p>Continent: {country.continents.continents}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
