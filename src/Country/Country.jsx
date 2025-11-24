cb2b34d7b94a1043a1ea72739def063e5a570947
arctic-shark <fahimshareear@gmail.com>
2025-11-25 02:35:34 +0600

    Adding conditional CSS class if visited

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -23,7 +23,7 @@ const Country = ({country}) => {
 
 
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h1>Name: {country.name.common}</h1>
             <h1>Official Name: {country.name.official}</h1>
